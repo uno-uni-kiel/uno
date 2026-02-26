ab22b5369e869b762aa1e02215a4886ca8065f3d
aimok04 <aimok04@mailbox.org>
2026-02-26 13:06:41 +0100

    added player creation form in home route

--- home.py
+++ home.py
@@ -1,5 +1,22 @@
-from flask import Request, render_template
+from flask import Request, render_template, request, session, redirect
 from sqlite3 import Connection, Cursor
 
-def handle_home(request: Request, con: Connection, cur: Cursor):
+def handle_home(con: Connection, cur: Cursor):
+    if request.method == "POST":
+        name = request.form["name"]
+        cur.execute('''
+            INSERT INTO spieler (name)
+            VALUES (?)
+            RETURNING id
+        ''', [name])
+
+        new_id = cur.fetchone()[0]
+        con.commit()
+
+        session["spieler_id"] = new_id
+        return redirect("dashboard")
+
+    if "spieler_id" in session:
+        return redirect("dashboard")
+
     return render_template("home.html")
