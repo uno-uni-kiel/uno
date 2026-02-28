6e04713731734fba230aaef7e911dae84f170e4e
aimok04 <aimok04@mailbox.org>
2026-02-28 13:59:11 +0100

    prettier script.js

--- static/script.js
+++ static/script.js
@@ -1,17 +1,17 @@
 async function checkRefresh() {
-    const refreshValue = await (await fetch("/refresh")).text()
-    console.log(refreshValue, window._oldRefreshValue)
-    if(refreshValue == window._oldRefreshValue) return
-    if(!window._oldRefreshValue) {
-        window._oldRefreshValue = refreshValue
-        return
-    }
+  const refreshValue = await (await fetch("/refresh")).text();
+  console.log(refreshValue, window._oldRefreshValue);
+  if (refreshValue == window._oldRefreshValue) return;
+  if (!window._oldRefreshValue) {
+    window._oldRefreshValue = refreshValue;
+    return;
+  }
 
-    window.location.href = window.location.href
+  window.location.href = window.location.href;
 }
 
 checkRefresh();
 
 setInterval(async () => {
-    checkRefresh()
+  checkRefresh();
 }, 1000);
